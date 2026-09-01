77b67dcb6bfb9813d5a3869cb011165f12abf885
polmechtech <ndakarton@gmail.com>
2026-09-01 17:19:39 +0200

    Improve organic SEO and Google product discovery

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/pila-formatowa-z-podcinaniem-do-plyt-meblowych-stol-wahadlowy-i-frezarski-2.html",
+        destination: "/kategoria/pila-formatowa",
+        permanent: true,
+      },
+      {
+        source: "/pila-formatowa-z-podcinaniem-stol-wahadlowy-oklieniarka.html",
+        destination: "/kategoria/pila-formatowa",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
