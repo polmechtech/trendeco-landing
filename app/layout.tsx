1c3aedb95ea292403606267b6b97af393673eca4
polmechtech <ndakarton@gmail.com>
2026-09-09 00:11:25 +0200

    Use non-www canonical domain in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
-const siteUrl = "https://www.trendeco.eu";
+const siteUrl = "https://trendeco.eu";
 const whatsappLink = "https://wa.me/48512077770?text=Dzień%20dobry%2C%20mam%20pytanie%20o%20ofertę%20TrendEco.";
 
 export const metadata: Metadata = {
